23bc378afbb834afa28d8b226f1ef3b1a409ebac
hta-axelor <hta.axelor@gmail.com>
2020-09-04 12:15:10 +0530

    Improve task editor

---
+++ axelor-project/src/main/webapp/project/task-editor/precache-manifest.bfc43c82ce80586034804a8dd52d6947.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44478f50ad75aabd9c2747842a5cfc6c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac55c5c395a2858b33e0",
+    "url": "./static/css/2.0b510fe8.chunk.css"
+  },
+  {
+    "revision": "a6c3c451d009163ca1cf",
+    "url": "./static/css/main.613d79ca.chunk.css"
+  },
+  {
+    "revision": "ac55c5c395a2858b33e0",
+    "url": "./static/js/2.cb3c5fc6.chunk.js"
+  },
+  {
+    "revision": "5a734cd1df47a9406c6392f11fd44928",
+    "url": "./static/js/2.cb3c5fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c3c451d009163ca1cf",
+    "url": "./static/js/main.a5167094.chunk.js"
+  },
+  {
+    "revision": "083e6c14fe78135d6547",
+    "url": "./static/js/runtime-main.298acfe2.js"
+  },
+  {
+    "revision": "5d75554dbc1274af3ce8f98a3ca41a50",
+    "url": "./static/media/user.5d75554d.jpg"
+  }
+]);
