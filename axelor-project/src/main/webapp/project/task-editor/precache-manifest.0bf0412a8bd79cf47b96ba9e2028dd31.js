52414e97f079d9e9030744a4376d3a2201c7b029
cma-axelor <c.malaviya@axelor.com>
2021-02-04 11:35:02 +0530

    Update build

---
+++ axelor-project/src/main/webapp/project/task-editor/precache-manifest.0bf0412a8bd79cf47b96ba9e2028dd31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44a487758199968133fe449773401db",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4a4ad485f250605fdde",
+    "url": "./static/css/2.0b510fe8.chunk.css"
+  },
+  {
+    "revision": "a16b210f08bd63e4e09a",
+    "url": "./static/css/main.613d79ca.chunk.css"
+  },
+  {
+    "revision": "b4a4ad485f250605fdde",
+    "url": "./static/js/2.66e6758e.chunk.js"
+  },
+  {
+    "revision": "8d22859a420ef73c278c92f2a492b1ef",
+    "url": "./static/js/2.66e6758e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16b210f08bd63e4e09a",
+    "url": "./static/js/main.601afd9a.chunk.js"
+  },
+  {
+    "revision": "083e6c14fe78135d6547",
+    "url": "./static/js/runtime-main.298acfe2.js"
+  },
+  {
+    "revision": "5d75554dbc1274af3ce8f98a3ca41a50",
+    "url": "./static/media/user.5d75554d.jpg"
+  }
+]);
--- axelor-project/src/main/webapp/project/task-editor/service-worker.js
+++ axelor-project/src/main/webapp/project/task-editor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4074a9ec385bc5eb4ba7a0b1af8cc601.js"
+  "./precache-manifest.0bf0412a8bd79cf47b96ba9e2028dd31.js"
 );
 
 self.addEventListener('message', (event) => {
